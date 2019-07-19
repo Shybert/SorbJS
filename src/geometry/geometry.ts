86b0acaab1e3b3e3dde0a53f1cf31b5300eadb9d
Shybert <25749775+Shybert@users.noreply.github.com>
2019-07-19 13:20:42 +0200

    Added simple point class and simple vector class

---
+++ src/geometry/geometry.ts
@@ -0,0 +1,21 @@
+export class Point {
+  x: number
+  y: number
+  z: number
+  constructor(x: number, y: number, z: number) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+}
+
+export class Vector {
+  x: number
+  y: number
+  z: number
+  constructor(x: number, y: number, z: number) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+}
