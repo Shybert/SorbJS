3c36d722e250993d87cf25878f3e36e8cc79e17f
Shybert <25749775+Shybert@users.noreply.github.com>
2019-07-26 21:49:02 +0200

    Slightly changed formula for calculating the cross product

--- src/geometry/math.ts
+++ src/geometry/math.ts
@@ -15,7 +15,7 @@ export function dot(vector1: Vector, vector2: Vector): number {
 export function cross(vector1: Vector, vector2: Vector): Vector {
   return new Vector(
     vector1.y * vector2.z - vector1.z * vector2.y,
-    -(vector1.x * vector2.z - vector1.z * vector2.x),
+    vector1.z * vector2.x - vector1.x * vector2.z,
     vector1.x * vector2.y - vector1.y * vector2.x
   )
 }
