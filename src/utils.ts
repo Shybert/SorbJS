62642a8d6aaa553e0797c53f7803d2a2d9e09dca
Shybert <25749775+Shybert@users.noreply.github.com>
2019-07-27 16:44:37 +0200

    Added the clampNumber() util

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function almostEquals(a: number, b: number) {
   return Math.abs(a - b) < 0.00001
 }
+
+export function clampNumber(number: number, min: number, max: number): number {
+  return number < min ? min : number > max ? max : number
+}
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { almostEquals } from '~utils'
+import { almostEquals, clampNumber } from '~utils'
 
 describe('almostEquals', () => {
   test('Should return true for equal numbers', () => {
@@ -17,5 +17,22 @@ describe('almostEquals', () => {
     expect(almostEquals(3, 4)).toBeFalsy()
     expect(almostEquals(-4, -4.1)).toBeFalsy()
     expect(almostEquals(1e5, 1.001e5)).toBeFalsy()
+  })
+})
+
+describe('clampNumber', () => {
+  test('Should return the number if the number is within the range', () => {
+    expect(clampNumber(3, 1, 4)).toBe(3)
+  })
+
+  test('Should return the min value if the number is less than min', () => {
+    expect(clampNumber(1, 2, 3)).toBe(2)
+    expect(clampNumber(-1, 0, 1)).toBe(0)
+    expect(clampNumber(1.33, 1.34, 2)).toBeCloseTo(1.34, 4)
+  })
+
+  test('Should return the max value if the number is larger than max', () => {
+    expect(clampNumber(42, 1, 2)).toBe(2)
+    expect(clampNumber(42.1, 0, 42.05)).toBeCloseTo(42.05, 4)
   })
 })
