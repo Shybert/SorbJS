48be23d148a0b726ccda76781e46eee88de75871
Shybert <25749775+Shybert@users.noreply.github.com>
2019-07-29 10:36:31 +0200

    Added matrixMultiplication()

--- src/geometry/math.ts
+++ src/geometry/math.ts
@@ -1,4 +1,4 @@
-import { Point, Vector } from '~src/geometry/geometry'
+import { Point, Vector, Matrix } from '~src/geometry/geometry'
 
 export function vectorBetween(point1: Point, point2: Point): Vector {
   return new Vector(
@@ -18,4 +18,18 @@ export function cross(vector1: Vector, vector2: Vector): Vector {
     vector1.z * vector2.x - vector1.x * vector2.z,
     vector1.x * vector2.y - vector1.y * vector2.x
   )
+}
+
+export function matrixMultiplication(a: Matrix, b: Matrix): Matrix {
+  const matrix = new Matrix()
+  for (let i = 0; i < 4; i += 1) {
+    for (let j = 0; j < 4; j += 1) {
+      matrix.matrix[i][j] =
+        b.matrix[0][j] * a.matrix[i][0] +
+        b.matrix[1][j] * a.matrix[i][1] +
+        b.matrix[2][j] * a.matrix[i][2] +
+        b.matrix[3][j] * a.matrix[i][3]
+    }
+  }
+  return matrix
 }
--- tests/geometry/math.test.ts
+++ tests/geometry/math.test.ts
@@ -1,5 +1,10 @@
-import { vectorBetween, dot, cross } from '~src/geometry/math'
-import { Point, Vector } from '~src/geometry/geometry'
+import {
+  vectorBetween,
+  dot,
+  cross,
+  matrixMultiplication
+} from '~src/geometry/math'
+import { Point, Vector, Matrix } from '~src/geometry/geometry'
 
 describe('vectorBetween', () => {
   test('Should give you the vector between two points', () => {
@@ -17,4 +22,23 @@ test('cross', () => {
   const vector2 = new Vector(2, 3, 4)
   expect(cross(vector1, vector2)).toEqualVector(new Vector(-1, 2, -1))
   expect(cross(vector2, vector1)).toEqualVector(new Vector(1, -2, 1))
+})
+
+test('matrixMultiplication', () => {
+  const a = new Matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
+  const b = new Matrix([
+    [-2, 1, 2, 3],
+    [3, 2, 1, -1],
+    [4, 3, 6, 5],
+    [1, 2, 7, 8]
+  ])
+
+  expect(matrixMultiplication(a, b)).toEqualMatrix(
+    new Matrix([
+      [20, 22, 50, 48],
+      [44, 54, 114, 108],
+      [40, 58, 110, 102],
+      [16, 26, 46, 42]
+    ])
+  )
 })
