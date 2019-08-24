e7edf770dacc87e16b8c75b242844b72272c8587
Shybert <25749775+Shybert@users.noreply.github.com>
2019-08-24 18:02:36 +0200

    Transform: Added the translation transformation

--- src/geometry/transform.ts
+++ src/geometry/transform.ts
@@ -1,3 +1,15 @@
 import { Matrix } from './geometry'
+import { matrixMultiplication } from './math'
 
-export class Transform extends Matrix {}
+export class Transform extends Matrix {
+  private multiplyAssign(matrix: Matrix): void {
+    this.matrix = matrixMultiplication(this, matrix).matrix
+  }
+
+  public translate(x: number, y: number, z: number): this {
+    this.multiplyAssign(
+      new Matrix([[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]])
+    )
+    return this
+  }
+}
--- tests/geometry/transform.test.ts
+++ tests/geometry/transform.test.ts
@@ -11,4 +11,23 @@ describe('Transform', () => {
     ])
     expect(transform).toEqualTransform(identityTransform)
   })
+
+  describe('translate', () => {
+    test('Should transform the transformation to a translation transformation', () => {
+      const transform = new Transform()
+      transform.translate(1, 2, 3)
+      const translationTransform = new Transform([
+        [1, 0, 0, 1],
+        [0, 1, 0, 2],
+        [0, 0, 1, 3],
+        [0, 0, 0, 1]
+      ])
+      expect(transform).toEqualTransform(translationTransform)
+    })
+
+    test('Should return the transformation', () => {
+      const transform = new Transform()
+      expect(transform.translate(1, 2, 3)).toBe(transform)
+    })
+  })
 })
