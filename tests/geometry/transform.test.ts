13f5816c3c0777ab6135ca3f0965a22eef25bc22
Shybert <25749775+Shybert@users.noreply.github.com>
2019-08-25 14:37:22 +0200

    Transform: Slightly reordered statements in tests for clarity

--- tests/geometry/transform.test.ts
+++ tests/geometry/transform.test.ts
@@ -15,13 +15,14 @@ describe('Transform', () => {
   describe('translate', () => {
     test('Should transform the transformation to a translation transformation', () => {
       const transform = new Transform()
-      transform.translate(1, 2, 3)
       const translationTransform = new Transform([
         [1, 0, 0, 1],
         [0, 1, 0, 2],
         [0, 0, 1, 3],
         [0, 0, 0, 1]
       ])
+
+      transform.translate(1, 2, 3)
       expect(transform).toEqualTransform(translationTransform)
     })
 
@@ -34,13 +35,14 @@ describe('Transform', () => {
   describe('scale', () => {
     test('Should transform the transformation to a scale transformation', () => {
       const transform = new Transform()
-      transform.scale(1, 2, 3)
       const scaleTransform = new Transform([
         [1, 0, 0, 0],
         [0, 2, 0, 0],
         [0, 0, 3, 0],
         [0, 0, 0, 1]
       ])
+
+      transform.scale(1, 2, 3)
       expect(transform).toEqualTransform(scaleTransform)
     })
 
