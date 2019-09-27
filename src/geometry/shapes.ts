31825034ee82a37f271163e60ded859e07bf4d2c
Shybert <25749775+Shybert@users.noreply.github.com>
2019-09-27 10:36:25 +0200

    Sphere: Changed intersect() to return an array of intersection objects, instead of the ray 't' values at each intersection
    The intersection object encapsulates the ray 't' values at intersection, in addition to the intersected object. This is needed to know which object has been intersected later in the rendering process.

--- src/geometry/shapes.ts
+++ src/geometry/shapes.ts
@@ -1,8 +1,17 @@
 import { Ray } from './ray'
 import { quadratic } from './math'
 
+type TShape = Sphere
+interface IIntersection {
+  t: number
+  object: TShape
+}
+function intersection(t: number, object: TShape): IIntersection {
+  return { t, object }
+}
+
 export class Sphere {
-  intersect(ray: Ray): number[] {
+  intersect(ray: Ray): IIntersection[] {
     const a = ray.direction.x ** 2 + ray.direction.y ** 2 + ray.direction.z ** 2
     const b =
       2 *
@@ -11,6 +20,6 @@ export class Sphere {
         ray.direction.z * ray.origin.z)
     const c = ray.origin.x ** 2 + ray.origin.y ** 2 + ray.origin.z ** 2 - 1
 
-    return quadratic(a, b, c)
+    return quadratic(a, b, c).map(t => intersection(t, this))
   }
 }
--- tests/geometry/shapes.test.ts
+++ tests/geometry/shapes.test.ts
@@ -2,47 +2,58 @@ import { Sphere } from '~src/geometry/shapes'
 import { Ray } from '~src/geometry/ray'
 import { Point, Vector } from '~src/geometry/geometry'
 
-describe('intersect', () => {
-  test('Intersecting at two points', () => {
-    const sphere = new Sphere()
-    const ray = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1))
-    const intersections = sphere.intersect(ray)
-    expect(intersections.length).toBe(2)
-    expect(intersections[0]).toBeCloseTo(4)
-    expect(intersections[1]).toBeCloseTo(6)
-  })
+describe('Sphere', () => {
+  describe('intersect', () => {
+    test('Intersecting at two points', () => {
+      const sphere = new Sphere()
+      const ray = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1))
+      const intersections = sphere.intersect(ray)
+      expect(intersections.length).toBe(2)
+      expect(intersections[0].t).toBeCloseTo(4)
+      expect(intersections[1].t).toBeCloseTo(6)
+    })
 
-  test('Intersecting at a tangent', () => {
-    const sphere = new Sphere()
-    const ray = new Ray(new Point(0, 1, -5), new Vector(0, 0, 1))
-    const intersections = sphere.intersect(ray)
-    expect(intersections.length).toBe(2)
-    expect(intersections[0]).toBeCloseTo(5)
-    expect(intersections[1]).toBeCloseTo(5)
-  })
+    test('Intersecting at a tangent', () => {
+      const sphere = new Sphere()
+      const ray = new Ray(new Point(0, 1, -5), new Vector(0, 0, 1))
+      const intersections = sphere.intersect(ray)
+      expect(intersections.length).toBe(2)
+      expect(intersections[0].t).toBeCloseTo(5)
+      expect(intersections[1].t).toBeCloseTo(5)
+    })
 
-  test('Not intersecting at all', () => {
-    const sphere = new Sphere()
-    const ray = new Ray(new Point(0, 2, -5), new Vector(0, 0, 1))
-    const intersections = sphere.intersect(ray)
-    expect(intersections.length).toBe(0)
-  })
+    test('No intersections', () => {
+      const sphere = new Sphere()
+      const ray = new Ray(new Point(0, 2, -5), new Vector(0, 0, 1))
+      const intersections = sphere.intersect(ray)
+      expect(intersections.length).toBe(0)
+    })
 
-  test('Intersecting both behind and in front of the origin', () => {
-    const sphere = new Sphere()
-    const ray = new Ray(new Point(0, 0, 0), new Vector(0, 0, 1))
-    const intersections = sphere.intersect(ray)
-    expect(intersections.length).toBe(2)
-    expect(intersections[0]).toBeCloseTo(-1)
-    expect(intersections[1]).toBeCloseTo(1)
-  })
+    test('Intersecting both behind and in front of the origin', () => {
+      const sphere = new Sphere()
+      const ray = new Ray(new Point(0, 0, 0), new Vector(0, 0, 1))
+      const intersections = sphere.intersect(ray)
+      expect(intersections.length).toBe(2)
+      expect(intersections[0].t).toBeCloseTo(-1)
+      expect(intersections[1].t).toBeCloseTo(1)
+    })
 
-  test('Both intersections behind the origin', () => {
-    const sphere = new Sphere()
-    const ray = new Ray(new Point(0, 0, 5), new Vector(0, 0, 1))
-    const intersections = sphere.intersect(ray)
-    expect(intersections.length).toBe(2)
-    expect(intersections[0]).toBeCloseTo(-6)
-    expect(intersections[1]).toBeCloseTo(-4)
+    test('Both intersections behind the origin', () => {
+      const sphere = new Sphere()
+      const ray = new Ray(new Point(0, 0, 5), new Vector(0, 0, 1))
+      const intersections = sphere.intersect(ray)
+      expect(intersections.length).toBe(2)
+      expect(intersections[0].t).toBeCloseTo(-6)
+      expect(intersections[1].t).toBeCloseTo(-4)
+    })
+
+    test('Returns the intersected object', () => {
+      const sphere = new Sphere()
+      const ray = new Ray(new Point(0, 0, -5), new Vector(0, 0, 1))
+      const intersections = sphere.intersect(ray)
+      expect(intersections.length).toBe(2)
+      expect(intersections[0].object).toBe(sphere)
+      expect(intersections[1].object).toBe(sphere)
+    })
   })
 })
