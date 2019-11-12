48c3fb97198787c343013cb95a17f54747089e99
Shybert <25749775+Shybert@users.noreply.github.com>
2019-11-12 19:51:04 +0100

    Created the "Red on Black" experiment, a simple rendering of a sphere
    * Correctly generates camera rays, accounting for both FoV and aspect ratio
    * Only supports rendering a single sphere
    * Has no sort of shading - if a ray hits, the pixel will be red, if not, the pixel will be black

---
+++ src/experiments/redOnBlack/redOnBlack.ts
@@ -0,0 +1,44 @@
+import { Point, Vector } from '~src/geometry/geometry'
+import { Canvas, Color, outputCanvas } from '~src/canvas'
+import { Ray } from '~src/geometry/ray'
+import { Sphere, hit } from '~src/geometry/shapes'
+
+const red = new Color(255, 0, 0)
+const black = new Color(0, 0, 0)
+
+class Camera {
+  canvas: Canvas
+  aspectRatio: number
+  fov: number
+  constructor(canvasWidth: number, canvasHeight: number, fov: number) {
+    this.canvas = new Canvas(canvasWidth, canvasHeight)
+    this.aspectRatio = canvasWidth / canvasHeight
+    this.fov = fov
+  }
+
+  render(sphere: Sphere): void {
+    for (let i = 0; i <= this.canvas.width; i += 1) {
+      for (let j = 0; j <= this.canvas.height; j += 1) {
+        const computedFov = Math.tan(this.fov / 2)
+        const x =
+          (-1 + 2 * ((i + 0.5) / this.canvas.width)) *
+          this.aspectRatio *
+          computedFov
+        const y = (1 - 2 * ((j + 0.5) / this.canvas.height)) * computedFov
+        const ray = new Ray(new Point(0, 0, 0), new Vector(x, y, 1))
+
+        const intersections = sphere.intersect(ray)
+        const rayHit = hit(intersections)
+        this.canvas.setPixel(i, j, rayHit ? red : black)
+      }
+    }
+  }
+}
+
+const sphere = new Sphere()
+sphere.transform.translate(0, 0, 2)
+const camera = new Camera(600, 600, Math.PI / 2)
+camera.render(sphere)
+outputCanvas(camera.canvas, document.getElementById(
+  'canvas'
+) as HTMLCanvasElement)
